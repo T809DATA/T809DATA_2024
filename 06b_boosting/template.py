6e7b581f436698c1489ad7b2c933c2faf3b03650
Hugo <hugo.cazaux@gmail.com>
2024-10-20 23:23:18 +0000

    Added grid search to Boosting

--- 06b_boosting/template.py
+++ 06b_boosting/template.py
@@ -1,3 +1,4 @@
+
 import os
 import numpy as np
 import pandas as pd
@@ -5,11 +6,9 @@ import matplotlib.pyplot as plt
 
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import (train_test_split,
-                                     RandomizedSearchCV)
+from sklearn.model_selection import (train_test_split, RandomizedSearchCV)
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
-from sklearn.metrics import (confusion_matrix, accuracy_score, recall_score,
-                             precision_score)
+from sklearn.metrics import (confusion_matrix, accuracy_score, recall_score, precision_score)
 
 from tools import get_titanic, build_kaggle_submission
 
@@ -43,6 +42,26 @@ def param_search(X, y):
     Perform randomized parameter search on the
     gradient boosting classifier on the dataset (X, y)
     '''
+    # Create the parameter grid
+    gb_param_grid = {
+        'n_estimators': [...],
+        'max_depth': [...],
+        'learning_rate': [...]}
+    # Instantiate the regressor
+    gb = GradientBoostingClassifier()
+    # Perform random search
+    gb_random = RandomizedSearchCV(
+        param_distributions=gb_param_grid,
+        estimator=gb,
+        scoring="accuracy",
+        verbose=0,
+        n_iter=50,
+        cv=4)
+    # Fit randomized_mse to the data
+    gb_random.fit(X, y)
+    # Print the best parameters and lowest RMSE
+    return gb_random.best_params_
+
 
 def gb_optimized_train_test(X_train, t_train, X_test, t_test):
     '''
